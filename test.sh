7621054602f6b6f81f3a07b5daa076f873f2c9cf
k-fog <59814110+k-fog@users.noreply.github.com>
2023-05-04 14:45:31 +0900

    update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-./pl0 '1 + 2'
-./pl0 '1 * 3'
-./pl0 '1 + 2 * 3'
-./pl0 '10 / 2 + 5'
-./pl0 '10 - 2 - 5'
-./pl0 '10 - (8 - 1)'
+function assert() {
+	expected=$1
+	input=$2
+	output=`./pl0 "$input"`
+	if [ "$output" = "$expected" ]; then
+		echo "{$input} => $output"
+	else
+		echo "expected $expected, but got $output"
+		exit 1
+	fi
+}
+
+assert 3 'return 1 + 2.'
+assert 3 'return 1 * 3.'
+assert 7 'return 1 + 2 * 3.'
+assert 10 'return 10 / 2 + 5.'
+assert 3 'return 10 - 2 - 5.'
+assert 3 'return 10 - (8 - 1).'
+assert 3 'return 4/2+4/4.'
+assert -5 'return 5-10.'
+assert 2 'return (1-(1-2)).'
+assert 0 'return (((0))).'
+assert 1 'return - -1.'
+assert 1 'if 1=1 then return 1.'
+
+echo done!
