8758ff667e2bc1a7c0c8aee26704318d84511a11
k-fog <59814110+k-fog@users.noreply.github.com>
2023-05-08 14:36:45 +0900

    インデント修正

--- tokenize.c
+++ tokenize.c
@@ -3,109 +3,109 @@
 Token *current_token;
 
 Token *new_token(TokenType type, Token *cur, char *str, int len) {
-    Token *tok = calloc(1, sizeof(Token));
-    tok->type = type;
-    tok->str = str;
-    tok->len = len;
-    cur->next = tok;
-    return tok;
+	Token *tok = calloc(1, sizeof(Token));
+	tok->type = type;
+	tok->str = str;
+	tok->len = len;
+	cur->next = tok;
+	return tok;
 }
 
 static bool startswith(char *p, char *q) {
-    return memcmp(p, q, strlen(q)) == 0;
+	return memcmp(p, q, strlen(q)) == 0;
 }
 
 Token *tokenize(char *src) {
-    Token head;
-    Token *cur = &head;
+	Token head;
+	Token *cur = &head;
 
-    while (*src) {
-        // skip space
-        if (isspace(*src)) {
-            src++;
-            continue;
-        }
+	while (*src) {
+		// skip space
+		if (isspace(*src)) {
+			src++;
+			continue;
+		}
 
-        // number
-        if (isdigit(*src)) {
-            char *tmp = src;
-            cur = new_token(TK_NUM, cur, src, 0);
-            cur->val = strtol(src, &src, 10);
-            cur->len = src - tmp;
-            continue;
-        }
+		// number
+		if (isdigit(*src)) {
+			char *tmp = src;
+			cur = new_token(TK_NUM, cur, src, 0);
+			cur->val = strtol(src, &src, 10);
+			cur->len = src - tmp;
+			continue;
+		}
 
-        // operator
-        if (startswith(src, "<>") || startswith(src, "<=") ||
-			startswith(src, ">=") || startswith(src, ":=")) {
-            cur = new_token(TK_OP, cur, src, 2);
-            src += 2;
-            continue;
-        }
-        if (strchr("+-*/=<>%", *src)) {
-            cur = new_token(TK_OP, cur, src++, 1);
-            continue;
-        }
+		// operator
+		if (startswith(src, "<>") || startswith(src, "<=") ||
+				startswith(src, ">=") || startswith(src, ":=")) {
+			cur = new_token(TK_OP, cur, src, 2);
+			src += 2;
+			continue;
+		}
+		if (strchr("+-*/=<>%", *src)) {
+			cur = new_token(TK_OP, cur, src++, 1);
+			continue;
+		}
 
 		// punctuator
-        if (strchr("(),;.", *src)) {
-            cur = new_token(TK_PUNCT, cur, src++, 1);
-            continue;
-        }
+		if (strchr("(),;.", *src)) {
+			cur = new_token(TK_PUNCT, cur, src++, 1);
+			continue;
+		}
 
-        // identifier
-        if (isalpha(*src)) {
-            char *start = src++;
-            while (isalpha(*src) || isdigit(*src) || *src == '_') src++;
-            cur = new_token(TK_IDENT, cur, start, src - start);
-            continue;
-        }
+		// identifier
+		if (isalpha(*src)) {
+			char *start = src++;
+			while (isalpha(*src) || isdigit(*src) || *src == '_') src++;
+			cur = new_token(TK_IDENT, cur, start, src - start);
+			continue;
+		}
 
 		exit(1);
-    }
+	}
 
-    new_token(TK_EOF, cur, src, 0);
+	new_token(TK_EOF, cur, src, 0);
 
-    return head.next;
+	return head.next;
 }
 
 char *to_string(char *s, int len) {
-    char *t = malloc(sizeof(char) * (len + 1));
-    strncpy(t, s, len);
-    return t;
+	char *t = malloc(sizeof(char) * (len + 1));
+	strncpy(t, s, len);
+	return t;
 }
 
 void view_tokens(Token *tok) {
 	printf("===== TOKENIZED =====\n");
-    for (;;) {
-        switch (tok->type) {
-            case TK_IDENT:
-                printf("identifier: {str: \"%s\", len: %d}\n", to_string(tok->str, tok->len), tok->len);
-                break;
-            case TK_OP:
-                printf("operator: {str: \"%s\", len: %d}\n", to_string(tok->str, tok->len), tok->len);
-                break;
-            case TK_PUNCT:
-                printf("punctuator: {str: \"%s\", len: %d}\n", to_string(tok->str, tok->len), tok->len);
-                break;
-            case TK_NUM:
-                printf("num: {val: %ld, len: %d}\n", tok->val, tok->len);
-                break;
-            case TK_EOF:
-                printf("EOF\n\n");
-                return;
-        }
-        tok = tok->next;
-    }
+	for (;;) {
+		switch (tok->type) {
+			case TK_IDENT:
+				printf("identifier: {str: \"%s\", len: %d}\n", to_string(tok->str, tok->len), tok->len);
+				break;
+			case TK_OP:
+				printf("operator: {str: \"%s\", len: %d}\n", to_string(tok->str, tok->len), tok->len);
+				break;
+			case TK_PUNCT:
+				printf("punctuator: {str: \"%s\", len: %d}\n", to_string(tok->str, tok->len), tok->len);
+				break;
+			case TK_NUM:
+				printf("num: {val: %ld, len: %d}\n", tok->val, tok->len);
+				break;
+			case TK_EOF:
+				printf("EOF\n\n");
+				return;
+		}
+		tok = tok->next;
+	}
 }
 
 Token *peek() {
-    return current_token;
+	return current_token;
 }
 
 Token *read() {
-    Token *t = current_token;
-    current_token = current_token->next;
-    return t;
+	Token *t = current_token;
+	current_token = current_token->next;
+	return t;
 }
 
