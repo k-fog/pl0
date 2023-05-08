f99cc700c803d0c33450aba7ad7d81b7a29e8699
k-fog <59814110+k-fog@users.noreply.github.com>
2023-05-08 13:39:31 +0900

    テストを追加

--- test.sh
+++ test.sh
@@ -34,6 +34,13 @@ assert 10 'function add(x,y) return x+y; return add(2,8).'
 assert 6 'function mul(x,y,z) return x*y*z; return mul(1,2,3).'
 assert 120 'function fact(n) begin if n<=1 then return 1; return n*fact(n-1); end; return fact(5).'
 assert 6765 'function fib(n) begin if n<=1 then return n; return fib(n-1)+fib(n-2); end; return fib(20).'
-assert 13 'function gcd(a,b) begin if a=0 then return b; return gcd(b%a,a); end; return gcd(13*4, 13*9).'
+assert 13 '
+function gcd(a,b)
+begin
+	if a = 0 then return b;
+	return gcd(b % a, a);
+end;
+return gcd(13*4, 13*9).'
+assert 20 'function a(x) return x; function b(x) return 2*a(x); return b(10).'
 
 echo OK!
